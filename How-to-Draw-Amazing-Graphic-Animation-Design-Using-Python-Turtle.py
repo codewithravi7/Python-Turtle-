933c2f6407aced43f0e4490f29711cb41d2fba5a
codewithravi7 <99835581+codewithravi7@users.noreply.github.com>
2023-11-01 11:48:10 +0530

    Create How-to-Draw-Amazing-Graphic-Animation-Design-Using-Python-Turtle.py
    source code add

---
+++ How-to-Draw-Amazing-Graphic-Animation-Design-Using-Python-Turtle.py
@@ -0,0 +1,18 @@
+
+#Code With Ravi
+
+import turtle 
+
+turtle.Screen().bgcolor("black")
+t=turtle.Turtle()
+t.pensize(3)
+t.speed(0)
+t.pencolor("red")
+for i in range(62):
+    t.left(59)
+    t.forward(150)
+    t.circle(88)
+    t.backward(75)
+    t.circle(44)
+    t.backward(75)
+
